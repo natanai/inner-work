d4479c0786e743bac08a989ad2dc1d8c3745e131
natanai <109430106+natanai@users.noreply.github.com>
2026-08-02 11:38:56 -0500

    Load sticky hand refinement styles

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import './tabletop/mobile-ux-declutter.css'
 import './tabletop/tactile-ux-pass.css'
 import './tabletop/sticky-strategy-hand.css'
 import './tabletop/sticky-strategy-hand-compat.css'
+import './tabletop/sticky-hand-refinement.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
